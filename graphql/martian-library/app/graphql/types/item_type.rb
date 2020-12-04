9511ba56279cd7bce4ff247b9de09e68efd0227d
Florian <florian.lecointe.etu@univ-lille.fr>
2020-12-04 17:01:17 +0100

    list of items

---
+++ graphql/martian-library/app/graphql/types/item_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class ItemType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: true
+    field :description, String, null: true
+    field :image_url, String, null: true
+  end
+end
