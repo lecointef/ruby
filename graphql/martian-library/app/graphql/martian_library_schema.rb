86ca24bae30fe74165264bef71a10fa185eb9355
Florian <florian.lecointe.etu@univ-lille.fr>
2020-12-07 20:10:45 +0100

    part two

--- graphql/martian-library/app/graphql/martian_library_schema.rb
+++ graphql/martian-library/app/graphql/martian_library_schema.rb
@@ -3,9 +3,9 @@ class MartianLibrarySchema < GraphQL::Schema
   query(Types::QueryType)
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
-  use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
+  #use GraphQL::Execution::Interpreter
+  #use GraphQL::Analysis::AST
 
   # Add built-in connections for pagination
-  use GraphQL::Pagination::Connections
+  #use GraphQL::Pagination::Connections
 end
